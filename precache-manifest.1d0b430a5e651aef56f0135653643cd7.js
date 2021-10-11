a86396a17dbecab25c424f1ad22b190dba613c82
Soralit <soralitria@gmail.com>
2021-10-11 17:25:12 +0800

    Updates

---
+++ precache-manifest.1d0b430a5e651aef56f0135653643cd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a3dc83d1dc574621a2dcf7a59a7955e",
+    "url": "/caravan/index.html"
+  },
+  {
+    "revision": "68f0ac7a1d20618f7fe5",
+    "url": "/caravan/static/css/2.f2850afe.chunk.css"
+  },
+  {
+    "revision": "0f043916913d3e997394",
+    "url": "/caravan/static/css/main.5954926c.chunk.css"
+  },
+  {
+    "revision": "68f0ac7a1d20618f7fe5",
+    "url": "/caravan/static/js/2.e5c89352.chunk.js"
+  },
+  {
+    "revision": "0b52b5e0beb68b4f3ea9dd25d3a4440b",
+    "url": "/caravan/static/js/2.e5c89352.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f043916913d3e997394",
+    "url": "/caravan/static/js/main.ed614590.chunk.js"
+  },
+  {
+    "revision": "463b8ec66a3715f618ea",
+    "url": "/caravan/static/js/runtime-main.7ddb6876.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/caravan/precache-manifest.9ff16d06434e7d7f75f892cc9243e234.js"
+  "/caravan/precache-manifest.1d0b430a5e651aef56f0135653643cd7.js"
 );
 
 self.addEventListener('message', (event) => {
